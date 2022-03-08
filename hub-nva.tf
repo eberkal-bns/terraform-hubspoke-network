3e514a5e28511262e12a6e8fb688daa0a66d0ddc
eberkal-bns <98850974+eberkal-bns@users.noreply.github.com>
2022-03-08 15:43:41 -0500

    Update hub-nva.tf

--- hub-nva.tf
+++ hub-nva.tf
@@ -14,81 +14,81 @@ resource "azurerm_resource_group" "hub-nva-rg" {
 }
 
 
-resource "azurerm_network_interface" "hub-nva-nic" {
-    name                 = "${local.prefix-hub-nva}-nic"
-    location             = azurerm_resource_group.hub-nva-rg.location
-    resource_group_name  = azurerm_resource_group.hub-nva-rg.name
-    enable_ip_forwarding = true
+#resource "azurerm_network_interface" "hub-nva-nic" {
+#    name                 = "${local.prefix-hub-nva}-nic"
+#    location             = azurerm_resource_group.hub-nva-rg.location
+#    resource_group_name  = azurerm_resource_group.hub-nva-rg.name
+#    enable_ip_forwarding = true
 
-    ip_configuration {
-    name                          = local.prefix-hub-nva
-    subnet_id                     = azurerm_subnet.hub-dmz.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.0.36"
-    }
+#    ip_configuration {
+#    name                          = local.prefix-hub-nva
+ #   subnet_id                     = azurerm_subnet.hub-dmz.id
+#    private_ip_address_allocation = "Static"
+#    private_ip_address            = "10.0.0.36"
+#    }
 
-    tags = {
-    environment = local.prefix-hub-nva
-    }
-}
+#    tags = {
+#    environment = local.prefix-hub-nva
+#    }
+#}
 
-resource "azurerm_virtual_machine" "hub-nva-vm" {
-    name                  = "${local.prefix-hub-nva}-vm"
-    location              = azurerm_resource_group.hub-nva-rg.location
-    resource_group_name   = azurerm_resource_group.hub-nva-rg.name
-    network_interface_ids = [azurerm_network_interface.hub-nva-nic.id]
-    vm_size               = var.vmsize
+#resource "azurerm_virtual_machine" "hub-nva-vm" {
+#    name                  = "${local.prefix-hub-nva}-vm"
+#    location              = azurerm_resource_group.hub-nva-rg.location
+#    resource_group_name   = azurerm_resource_group.hub-nva-rg.name
+#    network_interface_ids = [azurerm_network_interface.hub-nva-nic.id]
+#    vm_size               = var.vmsize
 
-    storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-    }
+#    storage_image_reference {
+#    publisher = "Canonical"
+ #   offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#    }
 
-    storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-    }
+#    storage_os_disk {
+#    name              = "myosdisk1"
+#    caching           = "ReadWrite"
+#    create_option     = "FromImage"
+#    managed_disk_type = "Standard_LRS"
+#    }
 
-    os_profile {
-    computer_name  = "${local.prefix-hub-nva}-vm"
-    admin_username = var.username
-    admin_password = var.password
-    }
+#    os_profile {
+#    computer_name  = "${local.prefix-hub-nva}-vm"
+#    admin_username = var.username
+#    admin_password = var.password
+#    }
 
-    os_profile_linux_config {
-    disable_password_authentication = false
-    }
+#    os_profile_linux_config {
+#    disable_password_authentication = false
+#    }
 
-    tags = {
-    environment = local.prefix-hub-nva
-    }
-}
+#    tags = {
+#    environment = local.prefix-hub-nva
+#    }
+#}
 
-resource "azurerm_virtual_machine_extension" "enable-routes" {
-    name                 = "enable-iptables-routes"
-    virtual_machine_id   = azurerm_virtual_machine.hub-nva-vm.id
-    publisher            = "Microsoft.Azure.Extensions"
-    type                 = "CustomScript"
-    type_handler_version = "2.0"
+#resource "azurerm_virtual_machine_extension" "enable-routes" {
+#    name                 = "enable-iptables-routes"
+#    virtual_machine_id   = azurerm_virtual_machine.hub-nva-vm.id
+#    publisher            = "Microsoft.Azure.Extensions"
+#    type                 = "CustomScript"
+#    type_handler_version = "2.0"
 
 
-    settings = <<SETTINGS
-    {
-        "fileUris": [
-        "https://raw.githubusercontent.com/mspnp/reference-architectures/master/scripts/linux/enable-ip-forwarding.sh"
-        ],
-        "commandToExecute": "bash enable-ip-forwarding.sh"
-    }
-SETTINGS
+#    settings = <<SETTINGS
+#    {
+#        "fileUris": [
+#        "https://raw.githubusercontent.com/mspnp/reference-architectures/master/scripts/linux/enable-ip-forwarding.sh"
+#        ],
+#        "commandToExecute": "bash enable-ip-forwarding.sh"
+#    }
+#SETTINGS
 
-    tags = {
-    environment = local.prefix-hub-nva
-    }
-}
+#    tags = {
+#    environment = local.prefix-hub-nva
+#    }
+#}
 
 
 #route table for Hub-Gateway
